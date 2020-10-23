4316ebbda2bdb29139a2723786d9ea815ba1882c
arcueku <59694020+arcueku@users.noreply.github.com>
2020-10-23 22:55:30 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// var val1 = "var変数";
+// console.log(val1);
+
+// // varは上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は際宣言可能
+// var val1 = "var変数を際宣言";
+// console.log(val1);
+
+// let val2="let変数"
+// console.log(val2);
+
+//  //letは上書き可能
+
+//  val2 ="let変数を上書き"
+//  console.log(val2);
+
+//  //letは際宣言不可能
+
+//  let val2 ="let変数を際宣言"
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// //const変数は上書き不可能
+// // val3 = "const変数を上書き";
+
+// //const変数は再定義不可能
+// const val3 = "const変数を再定義";
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "arcueku",
+//   age: 27
+// };
+
+// val4.name = "あるくえく";
+// val4.address = "Aich";
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bard";
+// val5.push("monkey")
+// console.log(val5);
